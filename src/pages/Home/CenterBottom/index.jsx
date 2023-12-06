85a513b4b12ae6c27d6b1514bd34a9e658f58088
haiyingshi <274976591@qq.com>
2023-12-06 18:51:05 +0800

    未来预测调试

--- src/pages/Home/CenterBottom/index.jsx
+++ src/pages/Home/CenterBottom/index.jsx
@@ -1,259 +1,323 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import * as echarts from 'echarts';
 import api from '@/api/index';
 import { Select } from 'antd';
 
-
-
 function CenterBottom() {
-	let [selectDate, setSelectDate] = useState('2023-12-05'); // 选择的日期
-	let [type, setType] = useState(24);
-	let [dataType, setDataType] = useState('usage');
-
+	let [selectDate, setSelectDate] = useState('2023-12-06'); // 选择的日期
+	let [type, setType] = useState('24小时');
+	let [dataType, setDataType] = useState('carbon');
 	let [intervalX, setIntervalX] = useState(2); // x轴间距是否隔开
-	let [chartOption, setChartOption] = useState({}); // option
-	let [chartXAxis, setChartXAxis] = useState([]); // x轴坐标数据
 	let [selectValue, setSelectValue] = useState('24小时'); // 下拉框数据
+	// select list
 	let [selectList, setSelectList] = useState(
 		[
 			{ value: '24小时', label: '24小时' },
 			{ value: '7天', label: '7天' }
 		]
-	); // select list
-	let [date1, setDate1] = useState([]); // 前面n天的日期
-	let [date2, setDate2] = useState([]);
+	);
 	let [beforeOneDay, setBeforeOneDay] = useState('');
 	let [afterOneDay, setAfterOneDay] = useState('');
 
 	let [dataList, setDataList] = useState({
-		prediction: [],
-		reality: []
-	}) 
-	// let [predictionList, setPredictionList] = useState([]); // 预测值
-	// let [realityList, setRealityList] = useState([]); // 实际值
-	let predictionListRef = useRef();
+		prediction: [], // 预测值
+		reality: [] // 实际值
+	})
 
 	useEffect(() => {
 		getPredict(selectDate, type, dataType);
-		
     }, [selectDate, type, dataType])
 
-	const init = function() {
-		var myChart = echarts.init(document.getElementById('centerBottomMain'));
-		myChart.clear();
-		setChartXAxis(() => {
-			if (date1 && date2) {
-				chartXAxis = [...date1, ...date2];
-				myChart.setOption(chartOption);
-			}
-		})
-	}
 
 	// 接口获取数据
 	const getPredict = async function(date, type, dataType) {
 		let params = {
 			day_str: date,
-			type: type == '24小时' ? 24 : 7,
+			type: type == '24小时' ? '24小时' : 7,
 			dataType,
 			park_id: 1,
-			unit: ''
+			unit: 'kgCO₂e'
 		}
-		await api.GetPredict(params).then(res=>{
-			let data = res;
-			let data1 = [];
-			let data2 = [];
-			data.forEach(el => {
-				if(el.name == '预测值') {
-					data1 = el.value;
-				} else {
-					data2 = el.value;
-				}
-			})
-			setDataList(() => {
-				getBeforeDate();
-				getEndDate()
-				getOption();
-				init();
-				return {
-					prediction: data1,
-					reality: data2
-				}
-				
-			})
+		const res = await api.GetPredict(params);
+		let obj = {};
+		res.forEach(el => {
+			if(el.name == '预测值') {
+				obj.prediction = el.value;
+			} else {
+				obj.reality = el.value;
+			}
 		})
+		setDataList(() => {
+			dataList = obj;
+			getOption();
+			return {...obj}
+		});
+		
 	}
 
 	// 获取option
 	const getOption = function() {
-		console.log(dataList.prediction)
-		if (!dataList.prediction || !dataList.reality) return;
-
+		let myChart = echarts.init(document.getElementById('centerBottomMain'));
 		let maxValue = Math.max.apply(null, [...dataList.prediction, ...dataList.reality]);
-		setChartOption(() => {
-			chartOption = {
-				tooltip: {
-					trigger: 'axis',
-					formatter: function (params) { // params 为一个数组，数组的每个元素 包含了 该折线图的点 所有的参数信息，比如 value(数值)、seriesName（系列名）、dataIndex（数据项的序号）
-						let dateIndex = 0; // 当前指示点的 日期序号
-						
-						let tipList = params.map((seg) => {
-							let { value, seriesName, dataIndex, color } = seg;
-							if (seriesName == 'name3' || seriesName == 'name4') return
-							dateIndex = dataIndex;
-							return `${getTipDot({ color })}${seriesName}：${value}`
-						})
+		let date1 = getBeforeDate();
+		let date2 = getEndDate();
+		// x 轴坐标
+		let chartXAxis = [...date1, ...date2];
 
-						tipList.unshift(`${chartXAxis[dateIndex]}`)
-						return tipList.join('<br/>')
+		let chartOption = {
+			tooltip: {
+				trigger: 'axis',
+				formatter: function (params) { // params 为一个数组，数组的每个元素 包含了 该折线图的点 所有的参数信息，比如 value(数值)、seriesName（系列名）、dataIndex（数据项的序号）
+					let dateIndex = 0; // 当前指示点的 日期序号
+					
+					let tipList = params.map((seg) => {
+						let { value, seriesName, dataIndex, color } = seg;
+						if (seriesName == 'name3' || seriesName == 'name4') return
+						dateIndex = dataIndex;
+						return `${getTipDot({ color })}${seriesName}：${value}`
+					})
+
+					tipList.unshift(`${chartXAxis[dateIndex]}`)
+					return tipList.join('<br/>')
+				}
+			},
+			legend: {
+				show: false
+			},
+			grid: {
+				left: '3%',
+				right: '3%',
+				top: '3%',
+				bottom: '15%',
+				containLabel: true
+			},
+			xAxis: [{
+				nameTextStyle: {
+					fontSize: 12,
+					padding: [30, 0, 0, 30]    // 四个数字分别为上右下左与原位置距离
+				},
+				type: 'category',
+				// 设置 x 轴的数据, 使用 日期 在数据中的 序列号 来表示 横坐标数值。
+				data: chartXAxis && chartXAxis.map((seg, index) => {
+					return index
+				}),
+				splitLine: {
+					show:false
+				},
+				axisLine: {
+					show: false,
+					lineStyle: {
+						color:'rgba(255, 255, 255, .87)'
 					}
 				},
-				legend: {
+				// 设置 x 轴的 展示标签， 使其根据 当前标签的序列号转换为 日期
+				axisLabel: {
+					formatter: function (params) {
+						return chartXAxis && chartXAxis[params]
+					},
+					color: 'rgba(255, 255, 255, .87)',
+					fontSize: 12,
+					interval: intervalX,
+				},
+				boundaryGap: false, // 两边的留白
+			},
+			{
+				type: 'value',
+				splitLine:{
 					show: false
 				},
-				grid: {
-					left: '3%',
-					right: '3%',
-					top: '3%',
-					bottom: '15%',
-					containLabel: true
+				min: 0,
+				max: chartXAxis && chartXAxis.length-1,
+				boundaryGap: false, // 两边的留白
+				axisPointer: { // tooltip不显示
+					type: 'none'
 				},
-				xAxis: [{
-					nameTextStyle: {
-						fontSize: 12,
-						padding: [30, 0, 0, 30]    // 四个数字分别为上右下左与原位置距离
+				show: false // 第二个x轴不显示
+			}],
+			yAxis: {
+				type: 'value',
+				position: 'right',
+				opacity: 1,
+				splitLine: {
+					show: true,
+					lineStyle: {
+						color: 'rgba(255, 255, 255, .12)'
+					}
+				},
+				axisLine: {
+					show: false,
+				},
+				axisTick: {
+					show: false
+				},
+				axisLabel: {
+					color: 'rgba(255, 255, 255, .87)',
+					interval: 0,
+					fontSize: 12
+				},
+			},
+			series: [
+				// 第一条线
+				{
+					z: -1,
+					name: "预测值",
+					type: "line",
+					smooth: false,
+					showSymbol: false,
+					data: dataList.prediction,
+					areaStyle: {
+						color: '#F04438',
+						opacity: .4,
+						origin: 'end',
 					},
-					type: 'category',
-					// 设置 x 轴的数据, 使用 日期 在数据中的 序列号 来表示 横坐标数值。
-					data: chartXAxis && chartXAxis.map((seg, index) => {
-						return index
-					}),
-					splitLine: {
-						show:false
-					},
-					axisLine: {
-						show: false,
-						lineStyle: {
-							color:'rgba(255, 255, 255, .87)'
-						}
-					},
-					// 设置 x 轴的 展示标签， 使其根据 当前标签的序列号转换为 日期
-					axisLabel: {
-						formatter: function (params) {
-							return chartXAxis && chartXAxis[params]
+					itemStyle: {
+						normal: {
+							color: 'rgba(240,68,56,.6)',
+							lineStyle: {
+								color: "#36BFFA",
+								type: 'dashed',
+								width: 2
+							},
 						},
-						color: 'rgba(255, 255, 255, .87)',
-						fontSize: 12,
-						interval: intervalX,
 					},
-					boundaryGap: false, // 两边的留白
+				},
+				// 第二条线
+				{
+					z: -1, // 使yAxis的splitLines显示在顶部
+					name: "实际值",
+					type: "line",
+					smooth: false,
+					showSymbol: false,
+					data: dataList.reality,
+					areaStyle: {
+						color: '#32D583',
+						opacity: .4,
+						origin: 'end',
+					},
+					itemStyle: {
+						normal: {
+							color: 'rgba(50,213,131,.6)',
+							lineStyle: {
+								color: "#36BFFA",
+								width: 2
+							},
+						},
+					},
 				},
 				{
-					type: 'value',
-					splitLine:{
-						show: false
-					},
-					min: 0,
-					max: chartXAxis && chartXAxis.length-1,
-					boundaryGap: false, // 两边的留白
-					axisPointer: { // tooltip不显示
-						type: 'none'
-					},
-					// show: false // 第二个x轴不显示
-				}],
-				yAxis: {
-					type: 'value',
-					position: 'right',
-					opacity: 1,
-					splitLine: {
-						show: true,
-						lineStyle: {
-							color: 'rgba(255, 255, 255, .12)'
-						}
-					},
-					axisLine: {
-						show: false,
-					},
-					axisTick: {
-						show: false
-					},
-					axisLabel: {
-						color: 'rgba(255, 255, 255, .87)',
-						interval: 0,
-						fontSize: 12
-					},
-				},
-				series: [
-					// 第一条线
-					{
-						z: -1,
-						name: "预测值",
-						type: "line",
-						smooth: false,
-						showSymbol: false,
-						data: dataList.prediction,
-						areaStyle: {
-							color: '#F04438',
-							opacity: .4,
-							origin: 'end',
-						},
+					type:'line',
+					smooth: false,
+					markLine: {
+						symbol: 'none',
+						data: [[
+							{coord:[date1 && (date1.length-1).toString(), 0]},
+							{coord:[date1 && (date1.length-1).toString(), maxValue]}
+						]],
 						itemStyle: {
 							normal: {
-								color: 'rgba(240,68,56,.6)',
 								lineStyle: {
-									color: "#36BFFA",
-									type: 'dashed',
-									width: 2
-								},
-							},
-						},
-					},
-					// 第二条线
-					{
-						z: -1, // 使yAxis的splitLines显示在顶部
-						name: "实际值",
-						type: "line",
-						smooth: false,
-						showSymbol: false,
-						data: dataList.reality,
-						areaStyle: {
-							color: '#32D583',
-							opacity: .4,
-							origin: 'end',
-						},
-						itemStyle: {
-							normal: {
-								color: 'rgba(50,213,131,.6)',
-								lineStyle: {
-									color: "#36BFFA",
-									width: 2
-								},
-							},
-						},
-					},
-					{
-						type:'line',
-						smooth: false,
-						markLine: {
-							symbol: 'none',
-							data: [[
-								{coord:[date1 && (date1.length-1).toString(), 0]},
-								{coord:[date1 && (date1.length-1).toString(), maxValue]}
-							]],
-							itemStyle: {
-								normal: {
-									lineStyle: {
-										width: 1,
-										color: '#DFDFDD',
-									}
+									width: 1,
+									color: '#DFDFDD',
 								}
-							},
-						}
+							}
+						},
 					}
-				]
+				}
+			]
+		}
+
+		// 计算虚线多出来的部分，前面用-占位
+		let lineData4 = [];
+		lineData4 = dataList.prediction.slice(dataList.reality.length-1)
+		for (let i=0; i < dataList.reality.length-1; i++ ) {
+			lineData4.unshift('-')
+		}
+
+		// 计算两个数据的最大值，组成二维数据
+		let line3Data = [];
+		dataList.prediction.forEach((item, i) => {
+			if (item - dataList.reality[i] >= 0) {
+				line3Data.push([i, item])
+			} else {
+				line3Data.push([i, dataList.reality[i]])
 			}
 		})
+
+		// 计算交点
+		let scatterData = getIntersectionPoint({
+			line1: dataList.prediction,
+			line2: dataList.reality,
+			date: chartXAxis
+		})
+		// 把交点添加到第三条线之间，形成另一条x轴的线
+		scatterData.forEach((el, index) => {
+			line3Data.forEach((item, i) => {
+				if(parseInt(el[0]) == item[0]) {
+					line3Data.splice(i+1, 0, el)
+				}
+			})
+		})
+
+		// 占位处理
+		line3Data.forEach((item, i) => {
+			if(!item[1]) {
+				item[1] = 0;
+			}
+		})
+
+		chartOption.series.push({
+			z:-1, // 使yAxis的splitLines显示在顶部 重要部分!!!
+			name: 'name3',
+			type: 'line',
+			smooth: false,
+			symbol: "none", // 去掉折线上面的小圆点
+			data: line3Data,
+			xAxisIndex: 1,
+			itemStyle: {
+				normal: {
+					lineStyle: {
+						color: "#36BFFA",
+						type: 'dashed', // 设置为虚线
+						width: .88
+					},
+				},
+			},
+			areaStyle: {
+				color: '#1e1e1e',
+				opacity: 1,
+				origin: 'end', // 填充折线外的区域
+			},
+		})
+
+		chartOption.series.push({
+			z: -1,
+			name: 'name4',
+			type: 'line',
+			smooth: false,
+			showSymbol: false,
+			data: lineData4,
+			itemStyle: {
+				normal: {
+					lineStyle: {
+						color: "#36BFFA",
+						type: 'dashed', // 设置为虚线
+						width: .1
+					},
+				},
+			},
+			areaStyle: {
+				color: '#1e1e1e',
+				opacity: 1,
+				origin: 'end', // 填充折线外的区域
+			},
+		})
+
+
+
+		myChart.setOption(chartOption);
 	}
 
+	// 获取前面n天的日期
 	const getBeforeDate = function() {
 		let arr = [];
 		if (selectValue != '24小时') {
@@ -288,14 +352,12 @@ function CenterBottom() {
 			}
 		} else {
 			// 24小时
-			for(let i = 0; i < 24; i++) {
+			for(let i = 0; i < 25; i++) {
 				arr.push(`${i}:00`)
 			}
+			arr[24] = '0:00';
 		}
-		setDate1((prevState) => {
-			date1 = arr;
-		})
-		
+		return arr;
 	}
 
 	// 获取未来n天的日期
@@ -328,9 +390,7 @@ function CenterBottom() {
 				arr.push(`${i}:00`)
 			}
 		}
-		setDate2((prevState) => {
-			date2 = arr;
-		})
+		return arr
 	}
 
 	const getTipDot = function({ radius = 5, color = "red" } = {}) {
@@ -339,11 +399,55 @@ function CenterBottom() {
 
 	// 切换下拉框内容
 	const handleChange = (value) => {
-		setType(() => {
-			type = value;
-			getPredict(selectDate, type, dataType)
-		});
-	};
+		setType(value);
+		setSelectValue(value);
+		value == '7天' ? setIntervalX(0) : setIntervalX(2);
+	}
+
+	// 计算两个线段交点，通过传入 两条线段、四个端点 的 横纵坐标 值，来计算两者交点的坐标
+	const segmentsIntr = function({ a, b, c, d } = {}) {
+		let denominator = (b.y - a.y) * (d.x - c.x) - (a.x - b.x) * (c.y - d.y)
+		let x = ((b.x - a.x) * (d.x - c.x) * (c.y - a.y) +
+			(b.y - a.y) * (d.x - c.x) * a.x -
+			(d.y - c.y) * (b.x - a.x) * c.x) / denominator;
+		let y = -((b.y - a.y) * (d.y - c.y) * (c.x - a.x) +
+			(b.x - a.x) * (d.y - c.y) * a.y -
+			(d.x - c.x) * (b.y - a.y) * c.y) / denominator;
+		return [x, y]
+	}
+
+	// 判断两条线段是否有交点, a1、b1 为两条线在 x1 处的值；a2、b2 为两条线在 x2 处的值；
+	// 只要不是一条线段的两个点都高于另一个点就会有交点；
+	const ifHaveIntersectionPoint = function(a1, b1, a2, b2) {
+		return (+a1 > +b1) != (+a2 > +b2)
+	}
+
+	// 是否执行后续的计算 ？ 不是最后一个点，且有交点时
+	const ifCalculatePoint = function(idx, lth, [a1, b1, a2, b2] = []) {
+		return idx !== (lth - 1) && ifHaveIntersectionPoint(a1, b1, a2, b2)
+	}
+
+	// 获取两线所有交点
+	const getIntersectionPoint = function({ line1, line2, date } = {}) {
+		// 交点数组
+		let intersectionPointList = []
+		date.map((seg, idx) => {
+			// 分别是两条线在相邻两处的数值，用于通过比较大小，来确定此段内是否有交点
+			let valueGroup = [line1[idx], line2[idx], line1[idx + 1], line2[idx + 1]]
+			if (ifCalculatePoint(idx, date.length, valueGroup)) {
+				let dotGroup = {
+					a: { x: idx, y: line1[idx] },
+					b: { x: idx + 1, y: line1[idx + 1] },
+					c: { x: idx, y: line2[idx] },
+					d: { x: idx + 1, y: line2[idx + 1] }
+				}
+				// 计算交点的位置
+				let intersectionPoint = segmentsIntr(dotGroup)
+				intersectionPointList.push(intersectionPoint)
+			}
+		})
+		return intersectionPointList;
+	}
 
 
     return (
