940e5f0d0d60d3a17863a317a4aea093d86f9c2f
haiyingshi <274976591@qq.com>
2024-02-02 18:22:17 +0800

    碳排放预测修改为tsx

--- src/pages/Home/RightBottom/index.tsx
+++ src/pages/Home/RightBottom/index.tsx
@@ -2,87 +2,11 @@ import { useState, useEffect } from 'react';
 import api from '@/api/index';
 import Echarts from '@/components/Echarts';
 
-
 function RightBottom(props: {date?: string}) {
 	let {date} = props;
+
 	let [option, setOption] = useState({});
 	let [server_id] = useState();
-	let [list, setList] = useState([]);
-	let [xAxisList, setXAxisList] = useState([]);
-
-	function init() {
-		// 绘制图表
-		setOption({
-			title: {
-				text: '节点包含设备数量',
-				textStyle: {
-					color: '#fff'
-				}
-			},
-			tooltip: {
-				trigger: 'axis',
-				axisPointer: {
-					type: 'shadow'
-				}
-			},
-			grid: {
-				left: '5%',
-				right: '5%',
-				top: '13%',
-				bottom: '3%',
-				containLabel: true
-			},
-			yAxis: {
-				type: 'value',
-				name: '数量',
-				nameTextStyle: {
-					fontSize: 12,
-				},
-				boundaryGap: [0, 0.01],
-				position: 'right',
-				splitLine: {
-					show: true,
-					lineStyle: {
-						color: 'rgba(255, 255, 255, .12)'
-					}
-				},
-				axisLine: {
-					show: false,
-				},
-				axisTick: {
-					show: false
-				},
-			},
-			xAxis: {
-				type: 'category',
-				axisLine: {
-					show: false
-				},
-				axisTick: {
-					show: false
-				},
-				axisLabel: {
-					color: 'rgba(255, 255, 255, .87)',
-					interval: 0,
-					fontSize: 12
-				},
-				data: xAxisList
-			},
-			series: [
-				{
-					name: '数量',
-					type: 'bar',
-					barWidth:'45%',
-					itemStyle:{
-						color:function(){
-							return '#2276FC'
-						}
-					},
-					data: list
-				}
-			]
-		});
-	}
 
 	async function getEquipmentByDay (server_id?: string, date?: string) {
 		await api.GetEquipmentByDay(server_id, date).then((res: any)=>{
@@ -92,13 +16,77 @@ function RightBottom(props: {date?: string}) {
 			arr.forEach((el: any) => {
 				xAxis.push(el.name);
 			});
-			setXAxisList([])
-			// setXAxisList(xAxis)
-			
-			// 异步执行
-			setList([])
-			// setList(...arr);
-			init();
+
+			setOption({
+				title: {
+					text: '节点包含设备数量',
+					textStyle: {
+						color: '#fff'
+					}
+				},
+				tooltip: {
+					trigger: 'axis',
+					axisPointer: {
+						type: 'shadow'
+					}
+				},
+				grid: {
+					left: '5%',
+					right: '5%',
+					top: '13%',
+					bottom: '3%',
+					containLabel: true
+				},
+				yAxis: {
+					type: 'value',
+					name: '数量',
+					nameTextStyle: {
+						fontSize: 12,
+					},
+					boundaryGap: [0, 0.01],
+					position: 'right',
+					splitLine: {
+						show: true,
+						lineStyle: {
+							color: 'rgba(255, 255, 255, .12)'
+						}
+					},
+					axisLine: {
+						show: false,
+					},
+					axisTick: {
+						show: false
+					},
+				},
+				xAxis: {
+					type: 'category',
+					axisLine: {
+						show: false
+					},
+					axisTick: {
+						show: false
+					},
+					axisLabel: {
+						color: 'rgba(255, 255, 255, .87)',
+						interval: 0,
+						fontSize: 12
+					},
+					data: xAxis
+				},
+				series: [
+					{
+						name: '数量',
+						type: 'bar',
+						barWidth:'45%',
+						itemStyle:{
+							color:function(){
+								return '#2276FC'
+							}
+						},
+						data: arr
+					}
+				]
+			});
 			
 		})
 	}
